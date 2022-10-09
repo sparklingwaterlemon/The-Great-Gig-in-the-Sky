1de4069d194e449aa806d5973a45f0c10ad32d1e
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-09 00:50:28 -0700

    handlScroll function

--- react-movie-lab/src/pages/App/App.jsx
+++ react-movie-lab/src/pages/App/App.jsx
@@ -15,6 +15,17 @@ function App() {
   // Handle Scroll Event
 
   // const [offsetY, setOffsetY]= useState(0);
+  const [offSetY, setOffSetY] = useState(0);
+
+  function handleScroll(){
+    setOffSetY(window.scrollY)
+    
+    console.log("test")
+    console.log(window.scrollY)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  })
 
 
   // const handleScroll = () => setOffsetY(window.pageYOffset);
@@ -31,7 +42,7 @@ function App() {
   // Handle Scroll Event
   // Handle Scroll Event
 
-  const [scrollingElement, setScrollingElement] = useState(0);
+  // const [scrollingElement, setScrollingElement] = useState(0);
   // const ScrollContext = createContext({
   //     scrollingElement: null,
   // });
@@ -59,18 +70,20 @@ function App() {
   const canvasRef = useRef(null)
   const canvas = canvasRef.current
 
+  // return import "./public/image"??
   const currentFrame = function(index){
     return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
   }
 
+  // 145 still images
+  const totalFrameCount = 145;
 
 
   useEffect(() => {
     const airPodImage = new Image();
-    airPodImage.src = currentFrame(1)
-    airPodImage.onload = () => setImage(airPodImage)
-    console.log("useEffect onload")   
-    console.log(airPodImage.height)
+    airPodImage.src = currentFrame(1);
+    airPodImage.onload = () => setImage(airPodImage);
+    console.log(airPodImage)   
   }, [])
 
   useEffect(() => {
