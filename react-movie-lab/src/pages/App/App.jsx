0d0162f1d8d1aebc77bc7e35f14567d7c0485737
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-09 20:30:18 -0700

    fixed Updating Images, basic prototype complete - moving onto importing airpod images into a public src file and updating images by acessing internal images vs external images

--- react-movie-lab/src/pages/App/App.jsx
+++ react-movie-lab/src/pages/App/App.jsx
@@ -4,63 +4,50 @@ import {useContext, useState, useEffect, useRef } from "react"
 
 function App() {
 
-  
 
-  
-  // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-   // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-    // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-     // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-      // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
+  // https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+  var html = document.documentElement;
+  // <html>
+  //   <head>
+  //   </head>
+  //   <body>
+  //   </body>
+  // </html>
+  var body = document.body;
+  // <body>
+  //   <main>
+  //     <div>
+  //     </div>
+  //   </main>
+  // </body>
 
-  // const [scrollYposition, setScrollYposition] = useState(0);
-  // const maxScrollHeight = 1379;
-        
+  console.log("---body.scrollheight----")
+  console.log(body.scrollHeight)
+  console.log("---body.offsetheight----")
+  console.log(body.offsetHeight)
+  console.log("---html.clientHeight----")
+  console.log(html.clientHeight)
+  console.log("---html.scrollHeight----")
+  console.log(html.scrollHeight)
+  console.log("---html.offsetHeight----")
+  console.log(html.offsetHeight)
 
-  // useEffect(() => {
-  //   const updatePosition = () =>{
-  //     setScrollYposition(window.scrollY)
-  //   };
-  //   window.addEventListener("scroll", updatePosition);
-
-  //   // removeEventListener added to prevent memory leak
-  //   return () => {window.removeEventListener("scroll", updatePosition)
-  //   console.log(scrollYposition)};
-  // }, [scrollYposition])
-  // CHANGE TO [ IMAGE] ???? LATER 
-
-        // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-      // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-    // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-  // SCROLL EFFECT EVENT V.1 ---- CAN BE BETTER
-
-
+  // getting the maximum height -- fix later if i want to add a sticky bottom scroll area
+  var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  console.log("---------")
+  console.log(height)
 
   const [scrollPosition, setScrollPosition] = useState(0);
-  const maxScrollHeight = 1505;
+  const maxScrollHeight = height;
 
-  const handleScroll = () => {
-      const position = window.scrollY;
-      setScrollPosition(position);
-  };
-
-  useEffect(() => {
-      // passive vs active listener???
-      window.addEventListener('scroll', handleScroll);
-      return () => window.removeEventListener('scroll', handleScroll);
-  }, [scrollPosition]);
-  // CHANGE ARRAY TO [IMAGE]??
-
-
-
-  
-
-  
+  // Scroll Fraction with give value between 0 and 1 to use for image
+  let scrollFraction = scrollPosition / maxScrollHeight
+  // console.log(scrollPosition)
+  // console.log(scrollFraction)
 
 
   const [image, setImage] = useState(null)
   const canvasRef = useRef(null)
-
 
   // return import "./public/image"??
   const currentFrame = function(index){
@@ -68,14 +55,50 @@ function App() {
   }
 
   // 145 still images
-  const totalFrameCount = 145;
+  const frameCount = 145;
 
+
+
+
+
+
+  const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+  };
 
   useEffect(() => {
-    const airPodImage = new Image();
-    airPodImage.src = currentFrame(1);
-    airPodImage.onload = () => setImage(airPodImage); 
-  }, [])
+      // passive ?? https://stackoverflow.com/questions/37721782/what-are-passive-event-listeners
+      window.addEventListener('scroll', handleScroll, {passive: true});
+      // remove memory leak
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      }
+  }, [scrollPosition]);
+  // CHANGE ARRAY TO [IMAGE]??
+
+
+
+  // image changer
+  useEffect(() => {
+      console.log("--------")
+      let scrollFraction = scrollPosition / maxScrollHeight
+      const frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount))
+      console.log("scrollPosition")
+      console.log(scrollPosition)
+      console.log("frame Index")
+      console.log(frameIndex)
+
+      const airPodImage = new Image();
+      airPodImage.src = currentFrame(frameIndex + 1);
+      airPodImage.onload = () => setImage(airPodImage); 
+  }, [scrollPosition])
+
+
+  
+
+
+
 
   useEffect(() => {
     if(image){
