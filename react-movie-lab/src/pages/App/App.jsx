48103730c1826585c6a2d731121f705b13b7780b
sparklingwaterlemon <michaelkim009@gmail.com>
2022-10-10 11:56:34 -0700

    (start) fetch api

--- react-movie-lab/src/pages/App/App.jsx
+++ react-movie-lab/src/pages/App/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import {useState, useEffect, useRef } from "react"
 import ParallaxEffect from '../ParallaxEffect/ParallaxEffect';
-import LoadImageTest from '../LoadImageTest/LoadImageTest';
+
+// testing components
+// load image test
+import LoadImageTest from '../Test-LoadImageTest/LoadImageTest';
+// fetch api test
+
 
 function App() {
   return (
