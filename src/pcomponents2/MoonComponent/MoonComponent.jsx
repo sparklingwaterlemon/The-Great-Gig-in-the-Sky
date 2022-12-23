fd3f810735559e74da8b08db751099e63828fc0c
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-22 18:30:21 -0800

    Resolved LowRes blurry image by declaring Canvas.width/height in MoonComponent.jsx vs .css file.

--- src/pcomponents2/MoonComponent/MoonComponent.jsx
+++ src/pcomponents2/MoonComponent/MoonComponent.jsx
@@ -57,6 +57,8 @@ export default function MoonComponent(){
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
+        canvas.width = 400;
+        canvas.height = 400;
         context.drawImage(image, 0, 0, canvas.width, canvas.height)
     },[image])
 
