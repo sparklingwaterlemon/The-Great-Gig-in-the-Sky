dad4aaa82de20e6985ee7b9e5305a0aa2a821e74
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-23 19:59:17 -0800

    ConstructionDisplay update

--- src/utilities/ConstructionDisplay/ConstructionDisplay.jsx
+++ src/utilities/ConstructionDisplay/ConstructionDisplay.jsx
@@ -9,8 +9,7 @@ export default function ConstructionDisplay(){
                 <span className="c-header">UNDER re-CONSTRUCTION</span>
                 <span className="c-date">LAST MODIFIED ON <strong>{text}</strong></span>
                 feel free to scroll
-                - reworking routes
-                - reworking organization/ structure of code
+                - recent update: preload images on load
                 <br/>
                 <a href="https://github.com/sparklingwaterlemon/The-Great-Gig-in-the-Sky" target="_blank" rel="noreferrer">github</a>
             </div>
