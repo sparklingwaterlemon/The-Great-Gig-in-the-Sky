edfe37f11f25dd2f7f8c5d43d41d54a704976b11
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-27 18:24:37 -0800

    removed borders

--- src/pages2/App/App.jsx
+++ src/pages2/App/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import { useEffect } from "react";
 
-// import AboutButton from "../../utilities/AboutButton/AboutButton";
 import MoonComponent from "../../pcomponents2/MoonComponent/MoonComponent";
 import ConstructionDisplay from "../../utilities/ConstructionDisplay/ConstructionDisplay";
 import PreloadImages from "../../utilities/PreLoadImages/PreLoadImages";
+// import AboutButton from "../../utilities/AboutButton/AboutButton";
 
 
-// import topImageDayNumber from "../../utilities/DayDateFunction/DayDateFunction";
-// import MOONIMAGES from "../../assets/MOONIMAGES/0000index";
-
-// import TestChildOne from "../../pages/TestChildOne/TestChildOne";
 
 export default function App(){
     PreloadImages();   
@@ -30,7 +26,6 @@ export default function App(){
             </div>
             <MoonComponent />
 
-            {/* <TestChildOne /> */}
             <div className="construction">
                 <ConstructionDisplay />
             </div>
