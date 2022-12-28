affeb96c92a653bab82970371f6dd1b194037b70
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-27 20:48:12 -0800

    button to scroll down...

--- src/utilities/ConstructionDisplay/ConstructionDisplay.jsx
+++ src/utilities/ConstructionDisplay/ConstructionDisplay.jsx
@@ -4,23 +4,24 @@ import "./ConstructionDisplay.css";
 export default function ConstructionDisplay(){
     let text = document.lastModified;
 
-    // useEffect(()=>{
-    //     var h = document.documentElement;
-    //     var msh = h.scrollHeight - h.clientHeight;
-    //     var sft = (msh/ 91) * 30.5; // 91 is the frame count // 30.5 is the 30th frame
-    //     var sft2 = Number(sft.toFixed(2));
-    //     window.scroll({
-    //         top: sft2,
-    //         left: 0,
-    //         behavior: "smooth",
-    //     });
-    //     console.log("RESTINGGG");
-    // },[]);
+    const scrollDown = () => {
+        var h = document.documentElement;
+        var msh = h.scrollHeight - h.clientHeight;
+        var sft = (msh/ 91) * 30.5; // 91 is the frame count // 30.5 is the 30th frame
+        var sft2 = Number(sft.toFixed(2));
+        window.scroll({
+            top: sft2,
+            left: 0,
+            behavior: "smooth",
+        });
+        console.log("RESTINGGG");
+    };
 
 
     return(
         <div className="c-border">
             <div className="c-container">
+                <button onClick={scrollDown}/>
                 <span className="c-header">UNDER re-CONSTRUCTION</span>
                 <span className="c-date">LAST MODIFIED ON <strong>{text}</strong></span>
                 feel free to scroll
