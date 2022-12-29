509f21f352a4bd2a1f64105ca8eda84f4add3a43
sparklingwaterlemon <michaelkim009@gmail.com>
2022-12-28 22:50:49 -0800

    wtf.. heroku

--- src/utilities/PostScroll/PostScroll.jsx
+++ src/utilities/PostScroll/PostScroll.jsx
@@ -35,7 +35,7 @@ export default function PostScroll(){
                 behavior: "smooth"
             });
             console.log("PostScroll--- Centered Out");
-        }, 2000);
+        }, 3000);
     };
 
     useEffect(()=>{
