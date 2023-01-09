fcea31424af49975489fabfdec98970a05864ce6
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-09 15:07:46 -0800

    There was a bug in getting the form evt.target.value.. had to recreate form. Bug gone.

--- src/pcomponents2/SearchBarComponent/SearchBarComponent.jsx
+++ src/pcomponents2/SearchBarComponent/SearchBarComponent.jsx
@@ -1,115 +1,24 @@
-// import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
+import "./SearchBarCSS.css";
 import { useState } from "react";
-import "./SearchBarComponent.css";
-
-//         <div className="search-bar-form-container">       
-//             <form onSubmit={handleFormSubmission}>
-//                 <input 
-//                     type="text" 
-//                     placeholder="Enter City or Zip Code" 
-//                     required  
-//                     value={location} 
-//                     onChange={handleFormInput}
-//                     className="search-input"
-//                 />
-//             </form>
-//         </div>      
-
-//     async function handleFormSubmission(evt){
-//         evt.preventDefault();
-
-//         await fetchDataFromAPI(location);
-    
-//         setLocation("")
-//     }
-
-
 
 export default function SearchBarComponent(){
-    const [searchLocation, setSearchLocation] = useState("");
+    const [zipcode, setZipcode] = useState("");
 
-    const handleInputForm=(evt)=>{
-        setSearchLocation(evt.target.value);
-    };
-    console.log(searchLocation);
-    
-    
-    const handleFormSubmit = async (evt) =>{
+    const handleFormInput = (evt) =>{
+        console.log(evt.target.value);
+        setZipcode(evt.target.value);
+    }
+
+    const handleFormSubmit = (evt) =>{
         evt.preventDefault();
-    } 
+        setZipcode("");
+    }
 
-
-
-    return (
-        <>
-        <div className="search-bar-container">
-            <form className="search-form" onSubmit={handleFormSubmit}>
-                <input
-                    type="text" placeholder="Enter City or Zip Code"
-                    onChange={handleInputForm} />
-                <button type="submit"> Submit </button>
+    return(
+        <div className="search-container">
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" placeholder="enter zip only" onChange={handleFormInput} value={zipcode} required />
             </form>
-
-            
-
-        </div>      
-        </>
+        </div>
     )
 }
-
-//     const [location, setLocation] = useState("");
-
-//     const FORECAST_CALL=`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&aqi=no&alerts=no`
-//     // const FORECAST_CALL=`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&aqi=no&alerts=no'
-
-//     function handleFormInput(evt){
-//         setLocation(evt.target.value)
-//     }
-
-//     async function fetchDataFromAPI(location){
-//         try{
-//             await fetch(FORECAST_CALL)
-//             .then(res => res.json())
-//             .then(data => {
-//                 setWeatherData(data)
-//             });
-//         } catch (error){
-//             console.log("error", error);
-//         }
-//     }
-
-//     async function handleFormSubmission(evt){
-//         evt.preventDefault();
-
-//         await fetchDataFromAPI(location);
-    
-//         setLocation("")
-//     }
-
- 
-    
-//     return (
-//         <>
-//         <div className="search-bar-form-container">
-            
-//             <form onSubmit={handleFormSubmission}>
-//                 <input 
-//                     type="text" 
-//                     placeholder="Enter City or Zip Code" 
-//                     required  
-//                     value={location} 
-//                     onChange={handleFormInput}
-//                     className="search-input"
-//                 />
-//             </form>
-
-//         </div>      
-
-
-//         </>   
-//     )
-// }
-
-
-
-
